beba756e38fc9bc37a1ac02e43df9052f4e2a5f4
Dusan Tadic <donjilapac@gmail.com>
2020-03-19 20:15:04 +0100

    Add toolbar

--- Coverage Dirs/Views/Cocoa/MainWindowController.swift
+++ Coverage Dirs/Views/Cocoa/MainWindowController.swift
@@ -13,7 +13,38 @@ class MainWindowController: NSWindowController {
     override func windowDidLoad() {
         super.windowDidLoad()
 
-        self.window?.registerForDraggedTypes([.fileURL])
+        self.window?.title = "Coverage Dirs"
+        self.window?.tabbingMode = .disallowed
+
+        let toolbar = NSToolbar(identifier: "mainToolbar")
+        toolbar.allowsUserCustomization = false
+        toolbar.delegate = self
+        self.window?.toolbar = toolbar
     }
 
+}
+
+extension MainWindowController: NSToolbarDelegate {
+    func toolbarAllowedItemIdentifiers(_ toolbar: NSToolbar)
+        -> [NSToolbarItem.Identifier] {
+        return [
+        .flexibleSpace,
+        .separator,
+        .space,
+        .toggleSidebar
+        ]
+    }
+
+    func toolbarDefaultItemIdentifiers(_ toolbar: NSToolbar)
+        -> [NSToolbarItem.Identifier] {
+        return [
+        .toggleSidebar
+        ]
+    }
+
+    func toolbar(_ toolbar: NSToolbar,
+                 itemForItemIdentifier itemIdentifier: NSToolbarItem.Identifier,
+                 willBeInsertedIntoToolbar flag: Bool) -> NSToolbarItem? {
+        return NSToolbarItem(itemIdentifier: itemIdentifier)
+    }
 }
